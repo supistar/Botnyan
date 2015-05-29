cef04ba9521f47b9aa9a3abffe8a4f49fe37ea0e
supistar <supistars@gmail.com>
2015-05-29 11:40:41 +0900

    Add new settings of USE_CACHE.

--- model/cache.py
+++ model/cache.py
@@ -4,6 +4,7 @@ import os
 import redis
 from redis.exceptions import ConnectionError
 
+import settings
 from model.exceptions import CacheError
 from model.metaclass.singleton import Singleton
 
@@ -19,30 +20,40 @@ class Cache():
         self.rd = redis.Redis(host=host, port=port, db=0)
 
     def set(self, key, value):
+        if not settings.USE_CACHE:
+            return
         try:
             self.rd.set(key, value)
         except ConnectionError as e:
             raise CacheError(e)
 
     def get(self, key):
+        if not settings.USE_CACHE:
+            return None
         try:
             return self.rd.get(key)
         except ConnectionError as e:
             raise CacheError(e)
 
     def contains(self, key):
+        if not settings.USE_CACHE:
+            return False
         try:
             return self.rd.exists(key)
         except ConnectionError as e:
             raise CacheError(e)
 
     def clear(self, key):
+        if not settings.USE_CACHE:
+            return
         try:
             self.rd.delete(key)
         except ConnectionError as e:
             raise CacheError(e)
 
     def keys(self):
+        if not settings.USE_CACHE:
+            return
         try:
             return self.rd.keys()
         except ConnectionError as e:
